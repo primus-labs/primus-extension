f6834d3aaa780420e291586830a5b18d3750b693
fksyuan <fksyuan@163.com>
2023-03-09 15:40:50 +0800

    add the home page communicate with background

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -23,3 +23,51 @@ const showIndex = (info, tab)=> {
   let url = chrome.runtime.getURL("home.html");
   chrome.tabs.create({ url });
 }
+
+// listen msg from extension tab page
+chrome.runtime.onConnect.addListener(
+  (port) => {
+    switch (port.name) {
+      case "invokealgo":
+        console.log("invokealgo connectted port=", port);
+        port.onMessage.addListener(processMsg);
+        break;
+      default:
+        break;
+    }
+  }
+);
+
+/*
+message : {
+  requestId,
+  algoMethod, // start or getData
+  algoParams { // this is getData params, start only proxyUrl
+    source,
+    host,
+    path,
+    appKey,
+    appSecret,
+    netMethod,
+    netParams
+  }
+}
+*/
+const processMsg = (message, port) => {
+  switch (message.algoMethod) {
+    case "start":
+      console.log("recv start=", message.algoParams.proxyUrl);
+      Module.ccall(
+        "myFunction", // name of C function
+        null, // return type
+        null, // argument types
+        null // arguments
+      );
+      port.postMessage({requestId: message.requestId, result: "success" });
+      break;
+    case "getData":
+      break;
+    default:
+      break;
+  }
+}
