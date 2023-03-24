ddc4bfe3edbc9e0e813e8d802b7452b4cb64ec38
yangshenmeng <17717039902@163.com>
2023-03-24 09:39:41 +0800

    style:fallback to the background original image

--- src/components/BackgroundAnimation/index.jsx
+++ src/components/BackgroundAnimation/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.sass'
-import bgLayer1 from '@/assets/img/bgLayer12.svg';
-import bgLayer2 from '@/assets/img/bgLayer22.svg';
-import bgLayer3 from '@/assets/img/bgLayer32.svg';
-import bgLayer4 from '@/assets/img/bgLayer42.svg';
+import bgLayer1 from '@/assets/img/bgLayer1.svg';
+import bgLayer2 from '@/assets/img/bgLayer2.svg';
+import bgLayer3 from '@/assets/img/bgLayer3.svg';
+import bgLayer4 from '@/assets/img/bgLayer4.svg';
 
 const BackgroundAnimation = (props) => {
   return (
