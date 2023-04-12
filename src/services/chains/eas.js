d1aa31e51b438148098a62d176728cc7e2f625d1
fksyuan <fksyuan@163.com>
2023-04-12 16:34:56 +0800

    add metamask code and eas sdk

---
+++ src/services/chains/eas.js
@@ -0,0 +1,22 @@
+import { EAS, Delegated, ATTEST_TYPE, SchemaEncoder, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+import { TypedDataEncoder } from "@ethersproject/hash";
+
+/*
+params = {
+    metamaskprovider: provider,
+    attesteraddr: addr,
+    apikey: apiKey
+}
+*/
+export async function getHash(params) {
+}
+
+/*
+params = {
+    metamaskprovider: provider,
+    attesteraddr: addr,
+    signature: signature
+}
+*/
+export async function attestByDelegation(params) {
+}
