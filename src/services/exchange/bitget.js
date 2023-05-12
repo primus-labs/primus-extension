9693deabe1c75e921d6ad2803a2231238d46cbca
fksyuan <fksyuan@163.com>
2023-05-12 15:47:14 +0800

    add biget

---
+++ src/services/exchange/bitget.js
@@ -0,0 +1,25 @@
+import BigNumber from 'bignumber.js';
+import Exchange from './exchange';
+import { gt } from '@/utils/utils';
+const BIGZERO = new BigNumber(0);
+
+class Bitget extends Exchange {
+  constructor(exchangeInfo) {
+    super('bitget', exchangeInfo);
+  }
+
+  async getTradingAccountTokenAmountMap() {
+    const res = await this.exchange.fetchBalance();
+    res.info.forEach(({ coinName, available, lock, freeze }) => {
+        // Tip: spot account balance = available + lock + freeze
+        const amt = add(add(available, lock), freeze).toFixed();
+        gt(amt, BIGZERO) && this.tradingAccountTokenAmountMap.set(coinName, amt);
+    });
+    console.log(
+       'bitget tradingAccountTokenAmountMap',
+       this.tradingAccountTokenAmountMap
+    );
+    return this.tradingAccountTokenAmountMap;
+  }
+}
+export default Bitget;
