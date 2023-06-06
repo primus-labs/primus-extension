d5beaa51601cfcde486f0577d8c2df9d3ec8cca3
ysm <ysm@ysm.com>
2023-06-06 15:04:54 +0800

    fix: submit on page lock when do not enter a password

--- src/pages/Lock/index.jsx
+++ src/pages/Lock/index.jsx
@@ -1,70 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 import rem from '@/utils/rem.js';
-import PInput from '@/components/PInput/index'
+import PInput from '@/components/PInput/index';
 import AsideAnimation from '@/components/Layout/AsideAnimation';
 import './index.sass';
-import { useSelector, useDispatch } from 'react-redux'
-import {postMsg} from '@/utils/utils'
+import { useSelector, useDispatch } from 'react-redux';
+import { postMsg } from '@/utils/utils';
 
 const Lock = () => {
-  const dispatch = useDispatch()
-  const padoServicePort = useSelector((state) => state.padoServicePort)
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const padoServicePort = useSelector((state) => state.padoServicePort);
+  const navigate = useNavigate();
   const [pwd, setPwd] = useState();
   const [errorMsg, setErrorMsg] = useState();
-  const handleClickStart = (val) => {
-    const curPwd = pwd ?? val
+  const handleClickStart = () => {
+    const curPwd = pwd;
+    if (!curPwd) {
+      setErrorMsg('Please enter your password');
+      return;
+    }
     dispatch({
       type: 'setUserPassword',
-      payload: curPwd
-    })
+      payload: curPwd,
+    });
     if (![undefined, null].includes(curPwd)) {
       const padoServicePortListener = function (message) {
         if (message.resMethodName === 'decrypt') {
-          console.log("page_get:decrypt:", message.res);
+          console.log('page_get:decrypt:', message.res);
           if (message.res) {
             // encrypt successfully
-            navigate('/?refreshData=true')
+            navigate('/?refreshData=true');
           } else {
-            setErrorMsg('Incorrect password')
+            setErrorMsg('Incorrect password');
           }
         }
-      }
-      padoServicePort.onMessage.addListener(padoServicePortListener)
+      };
+      padoServicePort.onMessage.addListener(padoServicePortListener);
       const msg = {
         fullScreenType: 'wallet',
         reqMethodName: `decrypt`,
         params: {
-          password: curPwd
-        }
-      }
-      postMsg(padoServicePort,msg)
+          password: curPwd,
+        },
+      };
+      postMsg(padoServicePort, msg);
     }
   };
   const handleChangePwd = (val) => {
-    setPwd(val)
-  }
-  const handleSubmitPwd = (val) => {
-    handleClickStart(val)
-  }
+    if (val && errorMsg && errorMsg === 'Please enter your password') {
+      setErrorMsg(undefined);
+    }
+    setPwd(val);
+  };
+  const handleSubmitPwd = () => {
+    handleClickStart();
+  };
   const handleClearUserPwd = () => {
     dispatch({
-      type:'setUserPassword',
-      payload: undefined
-    })
+      type: 'setUserPassword',
+      payload: undefined,
+    });
     const msg = {
       fullScreenType: 'wallet',
       reqMethodName: 'clearUserPassword',
-      params: {}
-    }
-    postMsg(padoServicePort,msg)
-  }
+      params: {},
+    };
+    postMsg(padoServicePort, msg);
+  };
 
   useEffect(() => {
     rem();
-    handleClearUserPwd()
+    handleClearUserPwd();
   }, []);
 
   return (
@@ -100,6 +107,4 @@ const Lock = () => {
   );
 };
 
-
-
-export default Lock
+export default Lock;
