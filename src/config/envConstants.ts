124784339a95ce93221643d7ba28aa5897f3a789
ysm <ysm@ysm.com>
2023-06-13 09:39:04 +0800

    update env configuration

--- src/config/envConstants.ts
+++ src/config/envConstants.ts
@@ -3,40 +3,68 @@ import iconPolygon from '@/assets/img/iconPolygon.svg';
 import iconArbitrum from '@/assets/img/iconArbitrum.svg';
 import iconOptimism from '@/assets/img/iconOptimism.svg';
 
-type ENVTYPE = 'development' | 'production';
+type ENVTYPE = 'development' | 'test' | 'production';
 
 // const CURENV = process.env.NODE_ENV as ENVTYPE;
 const CURENV = 'development';
-// console.log('CURENV', CURENV);
 const PADOURLMAP = {
   development: '18.179.8.186:8889',
-  //development: '127.0.0.1:8081',
+  test: '18.179.8.186:8888',
   production: '127.0.0.1:8081',
 };
 export const PADOURL = PADOURLMAP[CURENV];
 
 const PROXYURLMAP = {
   development: '18.179.8.186:9001',
-  //development: '127.0.0.1:9000',
+  test: '18.179.8.186:9000',
   production: '127.0.0.1:9000',
 };
 export const PROXYURL = PROXYURLMAP[CURENV];
 
 const PADOADDRESSMAP = {
   development: '0xe02bd7a6c8aa401189aebb5bad755c2610940a73',
+  test: '0xe02bd7a6c8aa401189aebb5bad755c2610940a73',
   production: '0x547E9fc83bE78bD656A20952EBE3bEBcc2ef8BD0',
 };
 export const PADOADDRESS = PADOADDRESSMAP[CURENV];
 
 const PADOSERVERURLMAP = {
   development: 'http://18.179.8.186:8080',
+  test: 'http://18.179.8.186:8080',
   production: 'http://18.179.8.186:8080',
 };
 export const PADOSERVERURL = PADOSERVERURLMAP[CURENV];
+// TODO!!!
 export const PADOSERVERURLHTTPS = 'https://18.179.8.186:8081';
 
 const EASINFOMAP = {
   development: {
+    Sepolia: {
+      icon: iconTool1,
+      title: 'Sepolia',
+      rpcUrl: 'https://sepolia.infura.io/v3/b6bf7d3508c941499b10025c0776eaf8',
+      easContact: '0xC2679fBD37d54388Ce493F1DB75320D236e1815e',
+      easProxyContrac: '0x2884e43b48c2cc623a19c0c3d260dd8f398fd5f3',
+      schemaUid:
+        '0x45316fbaa4070445d3ed1b041c6161c844e80e89c368094664ed756c649413a9',
+      chainId: '0xaa36a7', // numToHex
+      chainName: 'Sepolia',
+      rpcUrls: [
+        'https://sepolia.infura.io/v3/b6bf7d3508c941499b10025c0776eaf8',
+      ],
+      blockExploreUrls: ['https://sepolia.etherscan.io'],
+    },
+    Polygon: {
+      icon: iconPolygon,
+      title: 'Polygon',
+      disabled: true,
+      rpcUrl: '',
+      easContact: '',
+      easProxyContrac: '',
+      schemaUid: '',
+    },
+  },
+  test: {
     Sepolia: {
       icon: iconTool1,
       title: 'Sepolia',
@@ -103,7 +131,7 @@ const EASINFOMAP = {
 export const EASInfo = EASINFOMAP[CURENV];
 
 export const ONCHAINLIST = Object.values(EASInfo);
-console.log('ONCHAINLIST', ONCHAINLIST);
+// console.log('ONCHAINLIST', ONCHAINLIST);
 
 // [
 // {
