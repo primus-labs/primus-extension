70521096000b16f614aa563dd932512cc5764625
ysm <ysm@ysm.com>
2023-06-21 10:26:53 +0800

    optimizi:useAuthorization

--- src/components/Home/AuthDialog/index.tsx
+++ src/components/Home/AuthDialog/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect,memo } from 'react';
 import { useSelector } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 
 import PHeader from '@/components/Layout/PHeader';
 import PMask from '@/components/PMask';
@@ -8,8 +7,9 @@ import rightArrow from '@/assets/img/rightArrow.svg';
 
 import type { AuthSourcesItem, AuthSourcesItems } from '@/services/api/user';
 import type { UserState } from '@/store/reducers';
-import { getAuthUrl, postMsg } from '@/utils/utils';
+import { postMsg } from '@/utils/utils';
 import { DEFAULTAUTHSOURCELIST } from '@/config/constants';
+import useAuthorization from '@/hooks/useAuthorization';
 
 import './index.sass';
 
@@ -24,13 +24,12 @@ const AuthDialog: React.FC<authDialogProps> = memo(({ onClose, onSubmit }) => {
     DEFAULTAUTHSOURCELIST
   );
   const [activeSource, setActiveSource] = useState<string>();
-  const [authWindowId, setAuthWindowId] = useState<number>();
-  const [checkIsAuthDialogTimer, setCheckIsAuthDialogTimer] = useState<any>();
   const [errorTip, setErrorTip] = useState<string>();
 
   const padoServicePort = useSelector(
     (state: UserState) => state.padoServicePort
   );
+  const authorize = useAuthorization();
 
   const handleClickNext = () => {
     if (!activeSource) {
@@ -61,53 +60,7 @@ const AuthDialog: React.FC<authDialogProps> = memo(({ onClose, onSubmit }) => {
     padoServicePort.onMessage.addListener(padoServicePortListener);
     fetchAllOAuthSources();
   };
-  const fetchIsAuthDialog = (state: string, source: string) => {
-    const msg = {
-      fullScreenType: 'padoService',
-      reqMethodName: 'checkIsLogin',
-      params: {
-        state,
-        source,
-        data_type: 'LOGIN',
-      },
-    };
-    postMsg(padoServicePort, msg);
-    console.log('page_send:checkIsLogin request');
-  };
-  const createAuthWindowCallBack: (
-    state: string,
-    source: string,
-    window?: chrome.windows.Window | undefined
-  ) => void = (state, source, res) => {
-    const newWindowId = res?.id;
-    setAuthWindowId(newWindowId);
-    // console.log('create', newWindowId)
-    const timer = setInterval(() => {
-      fetchIsAuthDialog(state, source);
-    }, 1000);
-    setCheckIsAuthDialogTimer(timer);
-    const removeWindowCallBack = (windowId: number) => {
-      setAuthWindowId(undefined);
-      windowId === newWindowId && timer && clearInterval(timer);
-      padoServicePort.onMessage.removeListener(padoServicePortListener);
-    };
-    const padoServicePortListener = async function (message: any) {
-      if (message.resMethodName === 'checkIsLogin') {
-        console.log('page_get:checkIsLogin:', message.res);
-        if (message.res) {
-          // console.log('remove', newWindowId)
-          newWindowId &&
-            chrome.windows.get(newWindowId, {}, (win) => {
-              win?.id && chrome.windows.remove(newWindowId);
-            });
-          timer && clearInterval(timer);
-          onSubmit();
-        }
-      }
-    };
-    chrome.windows.onRemoved.addListener(removeWindowCallBack);
-    padoServicePort.onMessage.addListener(padoServicePortListener);
-  };
+  
   const handleClickOAuthSource = (item: AuthSourcesItem) => {
     const source = item.name;
     if (item.enabled !== '0') {
@@ -115,46 +68,15 @@ const AuthDialog: React.FC<authDialogProps> = memo(({ onClose, onSubmit }) => {
     }
     setActiveSource(source);
     setErrorTip(undefined);
-    // If the authorization window is open,focus on it
-    if (authWindowId) {
-      chrome.windows.update(authWindowId, {
-        focused: true,
-      });
-      return;
-    }
-    const state = uuidv4();
-    var width = 520;
-    var height = 620;
-    const windowScreen: Screen = window.screen;
-    var left = Math.round(windowScreen.width / 2 - width / 2);
-    var top = Math.round(windowScreen.height / 2 - height / 2);
-    const authUrl = getAuthUrl({
-      source,
-      state,
-    });
-    const windowOptions: chrome.windows.CreateData = {
-      url: authUrl,
-      type: 'popup',
-      focused: true,
-      // setSelfAsOpener: false,
-      top,
-      left,
-      width,
-      height,
-    };
-    chrome.windows.create(windowOptions, (window) => {
-      createAuthWindowCallBack(state, source, window);
-    });
+    const upperCaseSourceName = source.toUpperCase()
+    const dataType = 'LOGIN'
+    authorize(upperCaseSourceName, onSubmit, dataType);
   };
 
   useEffect(() => {
     padoServicePort && getAllOAuthSources();
   }, [padoServicePort]);
-  useEffect(() => {
-    return () => {
-      checkIsAuthDialogTimer && clearInterval(checkIsAuthDialogTimer);
-    };
-  }, [checkIsAuthDialogTimer]);
+  
 
   return (
     <PMask onClose={onClose}>
--- src/hooks/useAuthorization.ts
+++ src/hooks/useAuthorization.ts
@@ -1,17 +1,32 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import type { UserState } from '@/store/reducers'
+import type { UserState } from '@/store/reducers';
 import { postMsg, getAuthUrl } from '@/utils/utils';
-const useAuthorization = () => {
-  const padoServicePort = useSelector((state: UserState) => state.padoServicePort)
-  const [authWindowId, setAuthWindowId] = useState<number>()
-  const [checkIsAuthDialogTimer, setCheckIsAuthDialogTimer] = useState<any>()
 
-  const createAuthWindowCallBack: (state: string, source: string, window?: chrome.windows.Window | undefined, onSubmit?:()=>void) => void
-    = useCallback((state, source, res, onSubmit) => {
-      const newWindowId = res?.id
-      setAuthWindowId(newWindowId)
+type CreateAuthWindowCallBack = (
+  state: string,
+  source: string,
+  window?: chrome.windows.Window | undefined,
+  onSubmit?: () => void,
+  dataType?: string
+) => void;
+type OauthFn = (
+  source: string,
+  onSubmit?: () => void,
+  dataType?: string
+) => void;
+const useAuthorization = () => {
+  const padoServicePort = useSelector(
+    (state: UserState) => state.padoServicePort
+  );
+  const [authWindowId, setAuthWindowId] = useState<number>();
+  const [checkIsAuthDialogTimer, setCheckIsAuthDialogTimer] = useState<any>();
+
+  const createAuthWindowCallBack: CreateAuthWindowCallBack = useCallback(
+    (state, source, res, onSubmit, dataType = 'DATASOURCE') => {
+      const newWindowId = res?.id;
+      setAuthWindowId(newWindowId);
       // console.log('create', newWindowId)
       const fetchIsAuthDialog = (state: string, source: string) => {
         postMsg(padoServicePort, {
@@ -20,81 +35,85 @@ const useAuthorization = () => {
           params: {
             state,
             source,
-            data_type: 'DATASOURCE'
-          }
-        })
-        console.log("page_send:checkIsLogin request");
-      }
-      const timer = setInterval(() => { fetchIsAuthDialog(state, source) }, 1000)
-      setCheckIsAuthDialogTimer(timer)
+            data_type: dataType,
+          },
+        });
+        console.log('page_send:checkIsLogin request');
+      };
+      const timer = setInterval(() => {
+        fetchIsAuthDialog(state, source);
+      }, 1000);
+      setCheckIsAuthDialogTimer(timer);
       const removeWindowCallBack = (windowId: number) => {
-        setAuthWindowId(undefined)
-        windowId === newWindowId && timer && clearInterval(timer)
-        padoServicePort.onMessage.removeListener(padoServicePortListener)
-      }
+        setAuthWindowId(undefined);
+        windowId === newWindowId && timer && clearInterval(timer);
+        padoServicePort.onMessage.removeListener(padoServicePortListener);
+      };
       const padoServicePortListener = async function (message: any) {
         if (message.resMethodName === 'checkIsLogin') {
-          console.log("page_get:checkIsLogin:", message.res);
+          console.log('page_get:checkIsLogin:', message.res);
           if (message.res) {
-            if (message.params?.data_type === 'DATASOURCE') {
+            // debugger;
+            // if (message.params?.data_type === 'DATASOURCE') {
               // console.log('remove', newWindowId)
-              newWindowId && chrome.windows.get(
-                newWindowId,
-                {},
-                (win) => {
-                  win?.id && chrome.windows.remove(newWindowId)
-                },
-              )
-              timer && clearInterval(timer)
-              onSubmit && onSubmit()
+              newWindowId &&
+                chrome.windows.get(newWindowId, {}, (win) => {
+                  win?.id && chrome.windows.remove(newWindowId);
+                });
+              timer && clearInterval(timer);
+              onSubmit && onSubmit();
             }
-          }
+          // }
         }
+      };
+      chrome.windows.onRemoved.addListener(removeWindowCallBack);
+      padoServicePort.onMessage.addListener(padoServicePortListener);
+    },
+    [padoServicePort]
+  );
+  const handleClickOAuthSource: OauthFn = useCallback(
+    (source, onSubmit, dataType = 'DATASOURCE') => {
+      // If the authorization window is open,focus on it
+      if (authWindowId) {
+        chrome.windows.update(authWindowId, {
+          focused: true,
+        });
+        return;
       }
-      chrome.windows.onRemoved.addListener(removeWindowCallBack)
-      padoServicePort.onMessage.addListener(padoServicePortListener)
-    }, [ padoServicePort])
-  const handleClickOAuthSource = useCallback((source: string,onSubmit?:() => void) => {
-    // If the authorization window is open,focus on it
-    if (authWindowId) {
-      chrome.windows.update(
-        authWindowId,
-        {
-          focused: true
-        }
-      )
-      return
-    }
-    const state = uuidv4()
-    var width = 520;
-    var height = 620;
-    const windowScreen: Screen = window.screen
-    var left = Math.round((windowScreen.width / 2) - (width / 2));
-    var top = Math.round((windowScreen.height / 2) - (height / 2));
-    const authUrl = getAuthUrl({
-      source,
-      state,
-    });
-    const windowOptions: chrome.windows.CreateData = {
-      url: authUrl,
-      type: 'popup',
-      focused: true,
-      // setSelfAsOpener: false,
-      top,
-      left,
-      width,
-      height,
-    };
-    chrome.windows.create(windowOptions, (window) => { createAuthWindowCallBack(state, source, window,onSubmit) })
-  }, [authWindowId, createAuthWindowCallBack])
+      const state = uuidv4();
+      var width = 520;
+      var height = 620;
+      const windowScreen: Screen = window.screen;
+      var left = Math.round(windowScreen.width / 2 - width / 2);
+      var top = Math.round(windowScreen.height / 2 - height / 2);
+      const authUrl = getAuthUrl({
+        source,
+        state,
+      });
+      const windowOptions: chrome.windows.CreateData = {
+        url: authUrl,
+        type: 'popup',
+        focused: true,
+        // setSelfAsOpener: false,
+        top,
+        left,
+        width,
+        height,
+      };
+      chrome.windows.create(windowOptions, (window) => {
+        createAuthWindowCallBack(state, source, window, onSubmit, dataType);
+      });
+    },
+    [authWindowId, createAuthWindowCallBack]
+  );
 
   useEffect(() => {
     return () => {
-      checkIsAuthDialogTimer && clearInterval(checkIsAuthDialogTimer)
-    }
-  }, [checkIsAuthDialogTimer])
+      checkIsAuthDialogTimer && clearInterval(checkIsAuthDialogTimer);
+    };
+  }, [checkIsAuthDialogTimer]);
 
-  return handleClickOAuthSource
-}
+  return handleClickOAuthSource;
+};
 
-export default useAuthorization
+export default useAuthorization;
