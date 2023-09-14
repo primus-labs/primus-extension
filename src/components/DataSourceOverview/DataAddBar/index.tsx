271ff2da2c643700fc9933518f472202e6be8457
ysm <ysm@ysm.com>
2023-09-14 15:43:20 +0800

    ui: DataAddBar

--- src/components/DataSourceOverview/DataAddBar/index.tsx
+++ src/components/DataSourceOverview/DataAddBar/index.tsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 import './index.sass';
+import iconAddNew from '@/assets/img/iconAddNew.svg';
 interface DataAddBarProps {
   onClick: () => void;
 }
@@ -7,8 +8,9 @@ const DataAddBar: React.FC<DataAddBarProps> = memo(({ onClick }) => {
   return (
     <div className="dataAddBar">
       <button className="updateBtn" onClick={onClick}>
-        <div className="imgUpdate"></div>
-        <span>Add Data</span>
+        {/* <div className="imgUpdate"></div> */}
+        {/* <span>Add Data</span> */}
+        <img src={iconAddNew} alt="" />
       </button>
     </div>
   );
