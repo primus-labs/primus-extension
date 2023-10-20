ae243f0a1f2510bb947f20582eb8afc2a85ddaa0
fksyuan <fksyuan@163.com>
2023-10-20 11:46:45 +0800

    update manifest generate

--- webpack.config.js
+++ webpack.config.js
@@ -191,10 +191,10 @@ var options = {
               );
             } else {
               let jsonobj = JSON.parse(content.toString());
-              jsonobj.host_permissions.push('https://api-dev.padolabs.org/');
+              /*jsonobj.host_permissions.push('https://api-dev.padolabs.org/');
               jsonobj.host_permissions.push(
                 'https://xuda-note.oss-cn-shanghai.aliyuncs.com/'
-              );
+              );*/
               return Buffer.from(
                 JSON.stringify({
                   description: process.env.npm_package_description,
