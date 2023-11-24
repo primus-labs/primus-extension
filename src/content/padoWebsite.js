649a6b8e83750ad58dc2f95bb5a362d50694c0d3
ysm <ysm@ysm.com>
2023-11-24 18:38:37 +0800

    update content/padoWebsite

--- src/content/padoWebsite.js
+++ src/content/padoWebsite.js
@@ -1,16 +1,36 @@
-var entranceEl = document.querySelector('#LINEA_DEFI_VOYAGE_entrance');
-// window.PADO = 'pado'
+function createDomElement(html) {
+  var dom = new DOMParser().parseFromString(html, 'text/html');
+  return dom.body.firstElementChild;
+}
+var padoStr = `<div id="pado-extension-inject-el"></div>`;
+var injectEl = createDomElement(padoStr);
+document.body.appendChild(injectEl);
 
-if (entranceEl) {
-  entranceEl.onclick = (e) => {
-    e.preventDefault();
-    chrome.runtime.sendMessage({
-      type: 'padoWebsite',
-      name: 'createTab',
-      params: {
-        eventName: 'LINEA_DEFI_VOYAGE',
-      },
-    });
-    return;
-  };
+var EventsNavEl = document.querySelector('#EventsNav');
+// window.PADO = 'pado'
+const fn = () => {
+  var entranceEl = document.querySelector('#LINEA_DEFI_VOYAGE_entrance');
+  console.log('pado-extension-entranceEl', entranceEl);
+  if (entranceEl) {
+    entranceEl.onclick = (e) => {
+      e.preventDefault();
+      chrome.runtime.sendMessage({
+        type: 'padoWebsite',
+        name: 'createTab',
+        params: {
+          eventName: 'LINEA_DEFI_VOYAGE',
+        },
+      });
+      return;
+    };
+  }
+};
+fn();
+if (EventsNavEl) {
+  console.log('pado-extension-EventsNavEl', EventsNavEl);
+  EventsNavEl.addEventListener('click', () => {
+    setTimeout(() => {
+      fn();
+    }, 300);
+  });
 }
