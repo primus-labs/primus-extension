e0ff6078e3b613807086d46c8f7e519626c2741a
ysm <ysm@ysm.com>
2023-12-28 17:49:07 +0800

    feat:WalletConnect

--- src/components/PConnect/index.tsx
+++ src/components/PConnect/index.tsx
@@ -1,5 +1,12 @@
 import React, { useState, useMemo, memo, useCallback, useEffect } from 'react';
-import { useSelector, useDispatch, } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  useWeb3Modal,
+  useWeb3ModalProvider,
+  useWeb3ModalAccount,
+} from '@web3modal/ethers5/react';
+
+import { ethers } from 'ethers';
 import {
   // setConnectWalletDialogVisible1,
   connectWalletAsync,
@@ -17,7 +24,17 @@ import type { ActiveRequestType } from '@/types/config';
 import iconWallet from '@/assets/img/layout/iconWallet.svg';
 
 const PConnect = memo(() => {
-  const [connectWalletDialogVisible1, setConnectWalletDialogVisible1] = useState<boolean>(false);
+  // 4. Use modal hook
+  const { open } = useWeb3Modal();
+  const {
+    address: walletConnectAddress,
+    chainId,
+    isConnected,
+  } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  const [connectWalletDialogVisible1, setConnectWalletDialogVisible1] =
+    useState<boolean>(false);
   const [activeRequest, setActiveRequest] = useState<ActiveRequestType>();
   const [step, setStep] = useState<number>(1);
   const connectWalletDialogVisible = useSelector(
@@ -27,7 +44,7 @@ const PConnect = memo(() => {
     (state: UserState) => state.connectedWallet
   );
   const dispatch: React.Dispatch<any> = useDispatch();
-  
+
   const errorDescEl = useMemo(
     () => (
       <>
@@ -37,47 +54,54 @@ const PConnect = memo(() => {
     ),
     []
   );
-  const handleConnect = useCallback(
-    () => {
-      setConnectWalletDialogVisible1(true)
-      setStep(1)
-    },
-    []
-  );
+  const handleConnect = useCallback(() => {
+    setConnectWalletDialogVisible1(true);
+    setStep(1);
+  }, []);
   const handleCloseMask = useCallback(() => {
-    setConnectWalletDialogVisible1(false)
+    setConnectWalletDialogVisible1(false);
   }, []);
   const handleSubmitConnectWallet = useCallback(
     async (wallet?: WALLETITEMTYPE) => {
-      setActiveRequest({
-        type: 'loading',
-        title: 'Requesting Connection',
-        desc: 'Check MetaMask to confirm the connection.',
-      });
-      setStep(2);
-      const startFn = () => {
+      if (wallet?.name === 'MetaMask') {
         setActiveRequest({
           type: 'loading',
-          title: 'Sign the message',
-          desc: "PADO uses this signature to verify that you're the owner of this address.",
+          title: 'Requesting Connection',
+          desc: 'Check MetaMask to confirm the connection.',
         });
         setStep(2);
-      };
-      const errorFn = () => {
+        const startFn = () => {
+          setActiveRequest({
+            type: 'loading',
+            title: 'Sign the message',
+            desc: "PADO uses this signature to verify that you're the owner of this address.",
+          });
+          setStep(2);
+        };
+        const errorFn = () => {
+          setActiveRequest({
+            type: 'warn',
+            title: 'Unable to proceed',
+            desc: errorDescEl,
+          });
+        };
+        // connectFn(startFn, errorFn);
+        dispatch(connectWalletAsync(undefined, startFn, errorFn));
+      } else if (wallet?.name === 'WalletConnect') {
         setActiveRequest({
-          type: 'warn',
-          title: 'Unable to proceed',
-          desc: errorDescEl,
+          type: 'loading',
+          title: 'Requesting Connection',
+          desc: 'Check Your wallet to confirm the connection.',
         });
-      };
-      // connectFn(startFn, errorFn);
-      dispatch(connectWalletAsync(undefined,startFn, errorFn));
+        setStep(2);
+        open({view: 'WalletConnect'});
+      }
     },
-    [dispatch,errorDescEl]
+    [dispatch, errorDescEl, open]
   );
   const onSubmitProcessDialog = useCallback(() => {
     setStep(1);
-    setConnectWalletDialogVisible1(false)
+    setConnectWalletDialogVisible1(false);
   }, []);
   const checkIfHadBound = useCallback(async () => {
     const { connectedWalletAddress } = await chrome.storage.local.get([
@@ -92,10 +116,33 @@ const PConnect = memo(() => {
   }, [checkIfHadBound]);
   useEffect(() => {
     if (connectedWallet?.address) {
-      setConnectWalletDialogVisible1(false)
-      setStep(1)
+      setConnectWalletDialogVisible1(false);
+      setStep(1);
     }
   }, [connectedWallet?.address]);
+  const signFn = useCallback(async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(walletProvider);
+      console.log('222123provider', provider);
+
+      const signer = provider.getSigner();
+      console.log('222123signer', provider);
+      const signature = await signer?.signMessage('PADO Labs');
+      console.log('222123signature', signature);
+    } catch (e) {
+      console.log('e', e);
+    }
+  }, [walletProvider]);
+  useEffect(() => {
+    console.log(
+      '222123',
+      walletConnectAddress,
+      chainId,
+      isConnected,
+      walletProvider
+    );
+    isConnected && signFn();
+  }, [walletConnectAddress, signFn, chainId, isConnected, walletProvider]);
   return (
     <div className="PConnect">
       {connectedWallet?.address ? (
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import {
   createHashRouter,
   RouterProvider,
 } from "react-router-dom";
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react';
 import '@/assets/css/global.css';
 import routes from '@/router'
 import { Provider } from 'react-redux'
@@ -13,7 +14,39 @@ const router = createHashRouter(routes);
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
+// 1. Get projectId
+const projectId = '0dabe09aae6ef261e2a469af9ccb9b76';
 
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+const testnet = {
+  chainId: 534351,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://sepolia.scrollscan.com',
+  rpcUrl: 'https://sepolia-rpc.scroll.io',
+};
+
+
+// 3. Create modal
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com',
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [mainnet, testnet],
+  projectId,
+});
 console.log('Page initialization')
 
 // TODO 
