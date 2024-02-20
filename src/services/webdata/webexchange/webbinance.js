40f1f100ce26819d4b4c54c0eb08fb230cf77c46
fksyuan <fksyuan@163.com>
2024-02-20 13:42:50 +0800

    add web okx

---
+++ src/services/webdata/webexchange/webbinance.js
@@ -0,0 +1,106 @@
+import BigNumber from 'bignumber.js';
+import { add, gt } from '@/utils/utils';
+import {
+  USDT,
+  BUSD,
+  TUSD,
+  BTC,
+  LDO,
+  STABLETOKENLIST,
+} from '@/config/constants';
+import WebExchange from './webexchange';
+const BIGZERO = new BigNumber(0);
+const ONE = 1;
+const ZERO = 0;
+
+class WebBinance extends WebExchange {
+  constructor(exchangeInfo) {
+    super('binance', exchangeInfo);
+  }
+
+  async getFundingAccountTokenAmountMap() {
+    const res = await this.exchange.fetchBalance({ type: 'funding' });
+
+    res.info.forEach(({ asset, free, locked, freeze }) => {
+      // Tip: funding account balance = free + locked + freeze
+      const amt = add(add(free, locked), freeze).toFixed();
+      gt(amt, BIGZERO) && this.fundingAccountTokenAmountMap.set(asset, amt);
+    });
+    // console.log(
+    //   'fundingAccountTokenAmountMap',
+    //   this.fundingAccountTokenAmountMap
+    // );
+    return this.fundingAccountTokenAmountMap;
+  }
+
+  async getTradingAccountTokenAmountMap() {
+    const res = await this.exchange.fetchBalance({ type: 'trading' });
+    res.info.balances.forEach(({ asset, free, locked }) => {
+      // Tip: trading account balance = free + locked
+      const amt = add(free, locked).toFixed();
+      gt(amt, BIGZERO) &&
+        (!asset.startsWith('LD') || asset === LDO) &&
+        this.tradingAccountTokenAmountMap.set(asset, amt);
+    });
+    console.log(
+      'tradingAccountTokenAmountMap',
+      this.tradingAccountTokenAmountMap
+    );
+    return this.tradingAccountTokenAmountMap;
+  }
+
+  async getFlexibleAccountTokenAmountMap() {
+    const res = await this.exchange.fetchBalance({ type: 'savings' });
+    res.info.positionAmountVos.forEach(({ asset, amount }) => {
+      // Tip: trading account balance = free + locked
+      const amt = new BigNumber(amount).toFixed();
+      gt(amt, BIGZERO) && this.flexibleAccountTokenAmountMap.set(asset, amt);
+    });
+    // console.log(
+    //   'flexibleAccountTokenAmountMap',
+    //   this.flexibleAccountTokenAmountMap
+    // );
+    return this.flexibleAccountTokenAmountMap;
+  }
+
+  async getTokenPriceMap() {
+    await this.getTotalHoldingTokenSymbolList();
+    // transfrom 'X' to 'XUSDT' when you query X's price;
+    // ex: ETH => ETHUSDT,// binance=>'ETHUSDT',others:'ETH-USDT'
+    // price unit: USD
+    // coninbase need filter USD
+    let LPSymbols = this.totalHoldingTokenSymbolList
+      .filter((i) => !STABLETOKENLIST.includes(i))
+      .concat(BTC)
+      .map((j) => `${j}/${USDT}`);
+    let res;
+    //let errorSymbol;
+    try {
+      res = await this.exchange.fetchTickers();
+    } catch (e) {
+      console.log('fetchTickers error:', this.exName, e);
+      return;
+    }
+    //console.log('fetchTickers res:', this.exName, res);
+    this.tokenPriceMap = STABLETOKENLIST.reduce((prev, curr) => {
+      prev[curr] = ONE + '';
+      return prev;
+    }, {});
+    LPSymbols.forEach((lpsymbol) => {
+      const tokenSymbol = lpsymbol.replace(`/${USDT}`, '');
+      const BUSDLpsymbol = lpsymbol.replace(`${USDT}`, BUSD);
+      const TUSDLpsymbol = lpsymbol.replace(`${USDT}`, TUSD);
+      const last =
+        res[lpsymbol]?.last ||
+        res[BUSDLpsymbol]?.last ||
+        res[TUSDLpsymbol]?.last;
+      if (last) {
+        this.tokenPriceMap[tokenSymbol] = new BigNumber(last).toFixed();
+      } else {
+        this.tokenPriceMap[tokenSymbol] = ZERO + '';
+      }
+    });
+    return this.tokenPriceMap;
+  }
+}
+export default WebBinance;
---
+++ src/services/webdata/webexchange/webokx.js
@@ -0,0 +1,81 @@
+import WebExchange from './webexchange';
+
+class WebOKX extends WebExchange {
+  constructor(exchangeInfo) {
+    super('okx', exchangeInfo);
+  }
+
+  async getFundingAccountTokenAmountMap() {
+    const params = {};
+    params.url = "https://www.okx.com/v2/asset/balance/asset-overview?valuationUnit=USDT&transferFroms=180%2C6%2C20%2C18&limit=100&t=" + new Date().getTime();
+    params.method = "GET";
+    const res = await this.request(params);
+    res.data.funding.forEach(({ currency, balance }) => {
+      this.fundingAccountTokenAmountMap.set(currency, balance);
+    });
+    // console.log(
+    //   'okx fundingAccountTokenAmountMap',
+    //   this.fundingAccountTokenAmountMap
+    // );
+    return this.fundingAccountTokenAmountMap;
+  }
+
+  async getTradingAccountTokenAmountMap() {
+    const params = {};
+    params.url = "https://www.okx.com/v2/asset/balance/asset-overview?valuationUnit=USDT&transferFroms=180%2C6%2C20%2C18&limit=100&t=" + new Date().getTime();
+    params.method = "GET";
+    const res = await this.request(params);
+    res.data.trading.forEach(({ currency, balance }) => {
+      this.tradingAccountTokenAmountMap.set(currency, balance);
+      this.tradingAccountTokenAmountObj[currency] = balance;
+    });
+    // console.log(
+    //   'okx tradingAccountTokenAmountMap',
+    //   this.tradingAccountTokenAmountMap
+    // );
+    return this.tradingAccountTokenAmountMap;
+  }
+
+  async getTokenPriceMap() {
+    await this.getTotalHoldingTokenSymbolList();
+    // transfrom 'X' to 'XUSDT' when you query X's price;
+    // ex: ETH => ETHUSDT,// binance=>'ETHUSDT',others:'ETH-USDT'
+    // price unit: USD
+    // coninbase need filter USD
+    let LPSymbols = this.totalHoldingTokenSymbolList
+      .filter((i) => !STABLETOKENLIST.includes(i))
+      .concat(BTC)
+      .map((j) => `${j}-${USDT}`);
+    let res;
+    //let errorSymbol;
+    const params = {};
+    params.url = "https://www.okx.com/priapi/v5/market/tickers?instType=SPOT&t=" + new Date().getTime();
+    params.method = "GET";
+    try {
+      res = await this.request(params);
+      res.data.forEach((lp) => {
+        res[lp.instId] = lp.last;
+      });
+    } catch (e) {
+      console.log('fetchTickers error:', this.exName, e);
+      return;
+    }
+    //console.log('fetchTickers res:', this.exName, res);
+    this.tokenPriceMap = STABLETOKENLIST.reduce((prev, curr) => {
+      prev[curr] = ONE + '';
+      return prev;
+    }, {});
+    LPSymbols.forEach((lpsymbol) => {
+      const tokenSymbol = lpsymbol.replace(`/${USDT}`, '');
+      if (res[lpsymbol] && res[lpsymbol].last) {
+        const { last } = res[lpsymbol];
+        this.tokenPriceMap[tokenSymbol] = new BigNumber(last).toFixed();
+      } else {
+        this.tokenPriceMap[tokenSymbol] = ZERO + '';
+      }
+    });
+    return this.tokenPriceMap;
+  }
+}
+
+export default WebOKX;
