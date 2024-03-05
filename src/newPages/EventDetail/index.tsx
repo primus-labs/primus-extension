4e90e156c74e9f92c84a448514b484bf59e5e613
ysm <ysm@ysm.com>
2024-03-05 14:30:10 +0800

    newui: event detail page

--- src/newPages/EventDetail/index.tsx
+++ src/newPages/EventDetail/index.tsx
@@ -39,7 +39,7 @@ const stepList: StepItem[] = [
   {
     id: 1,
     title: 'Follow PADO social medial',
-    finished: true,
+    finished: false,
     tasksProcess: {
       total: 2,
       current: 0,
@@ -51,11 +51,19 @@ const stepList: StepItem[] = [
     title: 'Complete an attestation with a KYCed account on Binance',
 
     finished: false,
+    tasksProcess: {
+      total: 1,
+      current: 0,
+    },
   },
   {
     id: 3,
     title: 'Submit to Linea',
     finished: false,
+    tasksProcess: {
+      total: 1,
+      current: 0,
+    },
   },
   {
     id: 4,
@@ -65,122 +73,71 @@ const stepList: StepItem[] = [
   },
 ];
 
+
 const DataSourceItem = memo(() => {
+  
+  const questionMap = {
+    1: {
+      id: 1,
+      question: 'Does the connected wallet address matter?',
+      answer:
+        'The wallet address connected in the top right corner serves two key functions: it records your task complete process and determines your eligibility for events checking. Changing the connected wallet address will also impact the tasks process displayed in the task list. Make sure you connect the same wallet address here and on the Linea event page.',
+      expand: true,
+    },
+    2: {
+      id: 2,
+      question: 'What should I do if the attestation process fails? ',
+      answer:
+        'As the attestation is under MPC-TLS process, it often depends on your internet condition. A quick.solution is to connect with a new Wifi network/connection, or change to a different VPN node, if possible. If you get an error message with an error code, feel free to contact us in the Discord #help channel.',
+      expand: true,
+    },
+    3: {
+      id: 3,
+      question:
+        'What does the “not meet the uniqueness requirement” error message mean?',
+      answer:
+        "Due to Linea’s requirements, each Binance KYC'ed account should be linked to only one wallet address. If you encounter this error message, it could mean either:Your Binance account is already linked to a different wallet address.Your currently connected wallet address is already linked to another Binance account through attestation.",
+      expand: true,
+    },
+    4: {
+      id: 4,
+      question: 'Can I join this event with a different wallet address? ',
+      answer:
+        'Yes, you can join this event with different wallet address. When you change the connected wallet above the task list, the task status will reset and you can go through it with the newly connected wallet address. Remember, you also need to connect the new address on the BAS attestation alliance campaign page to earn your BAS XPS with this new address.',
+      expand: true,
+    },
+    5: {
+      id: 5,
+      question: 'How are PADO points for this event counted?',
+      answer:
+        'For PADO points, it is counted based on your PADO extension account, which means PADO points will not be double counted when you switch wallet address.',
+      expand: true,
+    },
+  };
   const [visibleAssetDialog, setVisibleAssetDialog] = useState<string>('');
   const [attestationPresets, setAttestationPresets] = useState<any>();
 
   const [visibleConnectByWeb, setVisibleConnectByAPI] =
     useState<boolean>(false);
+  const [questionList, setQuestionList] = useState<any[]>(
+    Object.values(questionMap)
+  );
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const eventId = searchParams.get('id') as string;
 
-  const webProofTypes = useSelector((state: UserState) => state.webProofTypes);
-  const {
-    metaInfo: activeDataSouceMetaInfo,
-    userInfo: activeDataSouceUserInfo,
-    // deleteFn: deleteDataSourceFn,
-  } = useDataSource(eventId);
-  const authorize = useAuthorization();
+  // const webProofTypes = useSelector((state: UserState) => state.webProofTypes);
+
   const dispatch: Dispatch<any> = useDispatch();
   const metaInfo = eventMetaMap[eventId];
+  const foramtQuestionList = useMemo(() => {
+    return [...Object.values(questionMap)]
+  }, [questionMap]);
 
-  const activeConnectType = useMemo(() => {
-    return activeDataSouceMetaInfo?.connectType;
-  }, [activeDataSouceMetaInfo]);
-
-  const hasConnected = useMemo(() => {
-    if (eventId === 'web3 wallet') {
-      return Object.values(activeDataSouceUserInfo).length > 0;
-    } else {
-      return activeDataSouceUserInfo?.name;
-    }
-  }, [activeDataSouceUserInfo]);
-
-  const btnTxtEl = useMemo(() => {
-    return activeConnectType ? 'Connect by ' + activeConnectType : 'Connect';
-  }, [activeDataSouceMetaInfo]);
-  const handleConnect = useCallback(async () => {
-    if (activeDataSouceMetaInfo.name === 'Web3 Wallet') {
-      await dispatch({ type: 'setRequireFetchAssets', payload: true });
-      dispatch(setConnectWalletDialogVisibleAction(true));
-      return;
-    }
-    if (activeConnectType === 'API') {
-      setVisibleConnectByAPI(true);
-    } else if (activeConnectType === 'Web') {
-      const currRequestObj = webProofTypes.find(
-        (r: any) => r.dataSource === eventId
-      );
-      // TODO-newui
-      if (eventId === 'tiktok') {
-        currRequestObj.datasourceTemplate.requests[0] = {
-          name: 'first',
-          url: 'https://www.tiktok.com/api/user/detail/',
-          queryParams: ['WebIdLastTime'],
-          method: 'GET',
-          headers: ['User-Agent'],
-          cookies: ['sessionid', 'tt-target-idc'],
-        };
-      }
-      // TODO END
-
-      // r.name === 'Account Ownership' &&
-      chrome.runtime.sendMessage({
-        type: 'dataSourceWeb',
-        name: 'init',
-        operation: 'connect',
-        params: {
-          ...currRequestObj,
-        },
-      });
-    } else if (activeConnectType === 'Auth') {
-      //  if (item.type === 'Social') {
-      var authorizeSourceKey = eventId.toUpperCase();
-      // if (authorizeSourceKey === 'G ACCOUNT') {
-      //   authorizeSourceKey = 'GOOGLE';
-      // }
-      authorize(authorizeSourceKey, () => {
-        dispatch(setSocialSourcesAsync());
-      });
-      //  } else if (item.type === 'Identity') {
-      //    // TODO
-      //    setActiveSource(item);
-      //    setStep(0);
-      //    setKYCDialogVisible(true);
-      //  }
-    }
-  }, [activeDataSouceMetaInfo, dispatch]);
   const handleBack = useCallback(() => {
     navigate(-1);
   }, [navigate]);
 
-  const handleSubmitConnectByAPI = useCallback(() => {
-    setVisibleConnectByAPI(false);
-  }, []);
-  const handleCloseAssetDialog = useCallback(() => {
-    setVisibleAssetDialog('');
-  }, []);
-  const handleSubmitAssetDialog = useCallback(() => {
-    setVisibleAssetDialog('');
-  }, []);
-  const handleAttest = useCallback((i) => {
-    setVisibleAssetDialog(i.attestationType);
-    const presetsP = Object.keys({
-      verificationContent: '',
-      verificationValue: '',
-      // account: ''
-    }).reduce(
-      (prev, curr) => {
-        if (i[curr]) {
-          prev[curr] = i[curr];
-        }
-        return prev;
-      },
-      { dataSourceId: eventId }
-    );
-    setAttestationPresets(presetsP);
-  }, []);
   const formatPeriod = (period) => {
     const { startTime, endTime } = period;
     const s = dayjs.utc(+startTime).format('MMM.D,YYYY');
@@ -188,6 +145,11 @@ const DataSourceItem = memo(() => {
 
     return `${s}-${e}`;
   };
+  const handleExpand = useCallback((i) => {
+    const { expand } = questionMap[i.id];
+    questionMap[i.id].expand = !expand;
+    setQuestionList(Object.values(questionMap));
+  }, []);
   return (
     <div className="pageEventDetail">
       <div className="pageContent">
@@ -247,19 +209,39 @@ const DataSourceItem = memo(() => {
                       </div>
                     )}
 
-                    <PButton
-                      text="Finish"
-                      type="primary"
-                      size="m"
-                      onClick={() => {}}
-                    />
+                    <PButton text="Finish" type="primary" onClick={() => {}} />
                   </div>
                 </li>
               );
             })}
           </ul>
         </div>
-        <div className="qs"></div>
+        <div className="qsList">
+          <h3 className="Frequently asked questions?"></h3>
+          <ul className="qss">
+            {questionList.map((i, k) => {
+              return (
+                <li className="qs" key={k}>
+                  <p className="question">
+                    <span>{i.question}</span>
+                    <PButton
+                      type="icon"
+                      icon={
+                        <i
+                          className={`iconfont ${
+                            i.expand ? 'icon-iconMinus' : 'icon-Add'
+                          } `}
+                        ></i>
+                      }
+                      onClick={() => handleExpand(i)}
+                    />
+                  </p>
+                  {i.expand && <p className="answer">{i.answer}</p>}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
