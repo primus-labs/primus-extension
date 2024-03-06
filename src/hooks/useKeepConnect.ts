e8b8fe28b62ed42d1c68628a01493772ac86232f
ysm <ysm@ysm.com>
2024-03-06 09:31:14 +0800

    newui: update useKeepConnect

--- src/hooks/useKeepConnect.ts
+++ src/hooks/useKeepConnect.ts
@@ -3,17 +3,33 @@ import { useDispatch, useSelector } from 'react-redux';
 import { postMsg } from '@/utils/utils';
 import useInterval from './useInterval';
 import type { UserState } from '@/types/store';
+import type { Dispatch } from 'react';
 
 const useKeepConnect = function useKeepConnect() {
+  const dispatch: Dispatch<any> = useDispatch();
+
   const padoServicePort = useSelector(
     (state: UserState) => state.padoServicePort
   );
-  const sendMsgFn = useCallback(() => {
-    const msg = {
-      fullScreenType: 'keepConnect',
+  // const sendMsgFn = useCallback(() => {
+  //   const msg = {
+  //     fullScreenType: 'keepConnect',
+  //   };
+  //   postMsg(padoServicePort, msg);
+  // }, [padoServicePort]);
+  // useInterval(sendMsgFn, 500, true, false);
+
+  const addDisconnectListener = useCallback(() => {
+    const onDisconnectFullScreen = (port: chrome.runtime.Port) => {
+      console.log('onDisconnectFullScreen port in page', port);
+      dispatch({
+        type: 'setPort',
+      });
     };
-    postMsg(padoServicePort, msg);
-  }, [padoServicePort]);
-  useInterval(sendMsgFn, 500, true, false);
+    padoServicePort.onDisconnect.addListener(onDisconnectFullScreen);
+  }, [dispatch, padoServicePort.onDisconnect]);
+  useEffect(() => {
+    addDisconnectListener();
+  }, [addDisconnectListener]);
 };
 export default useKeepConnect;
