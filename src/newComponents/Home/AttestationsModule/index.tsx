4be3aee5d5be254a2e3138ed3d3ed30460206085
ysm <ysm@ysm.com>
2024-03-27 18:24:10 +0800

    feat:home page

--- src/newComponents/Home/AttestationsModule/index.tsx
+++ src/newComponents/Home/AttestationsModule/index.tsx
@@ -1,12 +1,16 @@
 import React, { memo } from 'react';
+import useRewardsStatistics from '@/hooks/useRewardsStatistics';
+import useAttestationsStatistics from '@/hooks/useAttestationsStatistics';
 import ZkAttestations from './ZkAttestations';
 import Rewards from './Rewards';
 import './index.scss';
 const DataSourcesModule = memo(() => {
+  const { attestationsSubmitOnChainLen } = useAttestationsStatistics();
+  const { rewardsList } = useRewardsStatistics();
   return (
     <div className="attestationsModule">
-      <ZkAttestations />
-      <Rewards />
+      {attestationsSubmitOnChainLen > 0 && <ZkAttestations />}
+      {rewardsList.length > 0 && <Rewards />}
     </div>
   );
 });
--- src/newComponents/Home/DataSourcesModule/index.tsx
+++ src/newComponents/Home/DataSourcesModule/index.tsx
@@ -1,12 +1,16 @@
 import React, { memo } from 'react';
+import useAllSources from '@/hooks/useAllSources';
+import useSocialStatistic from '@/hooks/useSocialStatistic';
 import AssetsDistribution from './AssetsDistribution';
-import Social from './Social'
+import Social from './Social';
 import './index.scss';
 const DataSourcesModule = memo(() => {
+  const { socialDataSourcesLen } = useSocialStatistic();
+  const { sortedConnectedAssetsSourcesList } = useAllSources();
   return (
     <div className="dataSourcesModule">
-      <AssetsDistribution />
-      <Social/>
+      {sortedConnectedAssetsSourcesList.length > 0 && <AssetsDistribution />}
+      {socialDataSourcesLen > 0 && <Social />}
     </div>
   );
 });
