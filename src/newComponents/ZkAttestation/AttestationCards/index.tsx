6d2545b9e78979a17f5de2e54d52ed78e969cbdb
ysm <ysm@ysm.com>
2024-05-10 15:06:14 +0800

    fix: compatible with older versions attetstation account

--- src/newComponents/ZkAttestation/AttestationCards/index.tsx
+++ src/newComponents/ZkAttestation/AttestationCards/index.tsx
@@ -397,7 +397,7 @@ const Cards: React.FC<PDropdownProps> = memo(
                     <div className="descItems descItemsWithNumberValue">
                       <div className="descItem">
                         <div className="label">Data account</div>
-                        <div className="value">{i.account}</div>
+                        <div className="value">{i.account ?? i.sourceUseridHash}</div>
                       </div>
                       <div className="descItem">
                         <div className="label">Create address</div>
