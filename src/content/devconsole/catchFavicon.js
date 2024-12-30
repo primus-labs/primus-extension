b49ed1bd3788d263220526065ecbbc7030326f47
ysm <ysm@ysm.com>
2024-12-30 17:43:17 +0800

    fix:Capture the favicon

--- src/content/devconsole/catchFavicon.js
+++ src/content/devconsole/catchFavicon.js
@@ -1,35 +1,29 @@
-const shortcutIcon = document.querySelector('link[rel="shortcut icon"]');
-console.log('shortcutIcon', shortcutIcon);
-if (shortcutIcon) {
-  const faviconUrl = shortcutIcon.href;
-  console.log('Favicon URL:', faviconUrl);
+let shortcutIcon = null;
+let queryTimer = null;
+const queryFaviconFn = () => {
+  const clearIcon = document.querySelector(
+    'link[rel="shortcut icon"], link[rel="icon"]'
+  );
+  const fuzzyIcon = document.querySelectorAll('link[rel*="icon"]')?.[0];
+  shortcutIcon = clearIcon || fuzzyIcon;
+  if (shortcutIcon) {
+    console.log('shortcutIcon', shortcutIcon);
+    const faviconUrl = shortcutIcon.href;
+    chrome.runtime.sendMessage({
+      type: 'devconsole',
+      name: 'FAVICON_URL',
+      params: {
+        url: faviconUrl,
+      },
+    });
+    if (queryTimer) {
+      clearInterval(queryTimer);
+      queryTimer = null;
+    }
+  }
+};
 
-  // chrome.runtime.sendMessage({
-  //   type: 'devconsole',
-  //   name: 'FAVICON_URL',
-  //   params: {
-  //     url: faviconUrl,
-  //   },
-  // });
-
-  // window.postMessage(
-  //   {
-  //     target: 'devconsole',
-  //     origin: 'primusExtension',
-  //     name: 'FAVICON_URL',
-  //     params: {
-  //       url: faviconUrl,
-  //     },
-  //   },
-  //   '*'
-  // );
-  chrome.runtime.sendMessage({
-    type: 'devconsole',
-    name: 'FAVICON_URL',
-    params: {
-      url: faviconUrl,
-    },
-  });
-} else {
-  console.log('No shortcut icon found on this page.');
+queryFaviconFn();
+if (!shortcutIcon) {
+  queryTimer = setInterval(queryFaviconFn, 1000);
 }
