cbab65d2748e067d3531b76dd24ef73acd99395a
ysm <ysm@ysm.com>
2025-01-08 15:05:07 +0800

    feat: set conditions of sdk attestation

--- src/pages/Background/padoZKAttestationJSSDK/index.js
+++ src/pages/Background/padoZKAttestationJSSDK/index.js
@@ -217,11 +217,8 @@ export const padoZKAttestationJSSDKMsgListener = async (
             },
             []
           );
-          const opMap = {
-            string: 'REVEAL_STRING',
-            number: 'REVEAL_STRING',
-            boolean: 'REVEAL_STRING',
-          };
+          
+          
           const newResponses = dataSourceTemplateObj.reduce((prev, curr) => {
             const { responseTemplate } = curr;
             const subconditions = responseTemplate.reduce((prevS, currS) => {
@@ -231,19 +228,53 @@ export const padoZKAttestationJSSDKMsgListener = async (
                 fieldType,
                 feilds,
               } = currS;
-              const subconditionItem = {
-                field: expression,
-                op: opMap[feilds[0].DataType] || 'REVEAL_STRING',
-                reveal_id: feilds[0].key,
-                type: fieldType,
+              const opMap = {
+                string: 'REVEAL_STRING',
+                number: 'REVEAL_STRING',
+                boolean: 'REVEAL_STRING',
               };
+              let subconditionItem = {
+                field: expression,
+                // op: opMap[feilds[0].DataType] || 'REVEAL_STRING', // TODO ">"
+                // reveal_id: feilds[0].key, // required if type is REVEAL_STRING
+                // type: fieldType, // "FIELD_REVEAL" FIELD_VALUE  FIELD_RANGE
+              };
+              const subItemCondition = params.attRequest?.attConditions?.[0]?.find(
+                (i) => {
+                  return i.jsonPath === expression;
+                }
+              );
+              const handleREVEALFn = () => {
+                subconditionItem.op = 'REVEAL_STRING';
+                subconditionItem.type = 'FIELD_REVEAL';
+                subconditionItem.reveal_id = expression.replace(/\./g, '-');
+              };
+              if (subItemCondition) {
+                const { op, value } = subItemCondition;
+                subconditionItem.op = op;
+                if (['>', '>=', '=', '!=', '<', '<='].includes(op)) {
+                  subconditionItem.type = 'FIELD_RANGE';
+                  subconditionItem.value = value;
+                } else if (op === 'SHA256') {
+                  subconditionItem.type = 'FIELD_VALUE';
+                } else if (op === 'REVEAL_STRING') {
+                  handleREVEALFn();
+                }
+              } else {
+                handleREVEALFn()
+              }
+              // TODO
+              // field: '$.data.create_time';
+              // op: '>';
+              // type: 'FIELD_RANGE';
+              // value: '978278400';
               prevS.push(subconditionItem);
               return prevS;
             }, []);
             let responseItem = {
               conditions: {
                 type: 'CONDITION_EXPANSION',
-                op: '&',
+                op: 'BOOLEAN_AND',
                 subconditions,
               },
             };
