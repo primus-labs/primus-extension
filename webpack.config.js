32ed2a0303dd9eb35a94d87d98e564e32b7e6f8b
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-02-21 11:35:33 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -529,7 +529,7 @@ var options = {
     //   include: /src/,
     //   // add errors to webpack instead of warnings
     //   failOnError: true,
-    //   // allow import cycles that include an asyncronous import,
+    //   // allow import cycles that include an asynchronous import,
     //   // e.g. via import(/* webpackMode: "weak" */ './file.js')
     //   allowAsyncCycles: false,
     //   // set the current working directory for displaying module paths
