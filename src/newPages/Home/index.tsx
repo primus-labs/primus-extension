3495ae78a83917be901e1880a53d44af0a9d457c
ysm <ysm@ysm.com>
2025-10-15 16:11:30 +0800

    feat: remove guide pages

--- src/newPages/Home/index.tsx
+++ src/newPages/Home/index.tsx
@@ -9,6 +9,9 @@ import AttestationsModule from '@/newComponents/Home/AttestationsModule';
 import './index.scss';
 import { useSelector } from 'react-redux';
 import { UserState } from '@/types/store';
+import useCreateAccount from '@/hooks/useCreateAccount';
+import useListener from '@/hooks/useListener';
+import { postMsg } from '@/utils/utils';
 
 const Home = memo(() => {
   const [showInputPasswordDialog, setShowInputPasswordDialog] =
@@ -24,6 +27,35 @@ const Home = memo(() => {
   useEffect(() => {
     checkIfHadSetPwd();
   }, [checkIfHadSetPwd]);
+
+  const { createAccountFn } = useCreateAccount();
+  useListener();
+  const padoServicePort = useSelector((state: any) => state.padoServicePort);
+
+  const initAccount = useCallback(async () => {
+    const { keyStore, padoCreatedWalletAddress, privateKey, userInfo } =
+      await chrome.storage.local.get([
+        'keyStore',
+        'padoCreatedWalletAddress',
+        'privateKey',
+        'userInfo',
+      ]);
+    if (!privateKey && !keyStore && !userInfo) {
+      const msg = {
+        fullScreenType: 'wallet',
+        reqMethodName: 'create',
+        params: {},
+      };
+      postMsg(padoServicePort, msg);
+    }
+    if (privateKey && !userInfo) {
+      createAccountFn();
+    }
+  }, []);
+
+  useEffect(() => {
+    initAccount();
+  }, [initAccount]);
   return (
     <div className="pageHome">
       <div className="pageContent">
