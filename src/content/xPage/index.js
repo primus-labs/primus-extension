0ab7c1de9374b8c30f995ebd703e60ede88efc48
ysm <ysm@ysm.com>
2025-10-20 17:06:11 +0800

    feat: cancel x task information

--- src/content/xPage/index.js
+++ src/content/xPage/index.js
@@ -1,35 +1,43 @@
+const xEventMap = {
+  '/intent/retweet': {
+    searchParamKey: 'tweet_id',
+    eventName: 'repost',
+  },
+  '/intent/follow': {
+    searchParamKey: 'screen_name',
+    eventName: 'follow',
+  },
+};
+let type = 'xPage';
 let checkTimer = null;
+
 checkTimer = setInterval(() => {
   var element = document.querySelector(
     "[data-testid='confirmationSheetConfirm']"
   );
-  console.log('primus injected xPage checkTimer follow btn', element);
-  if (element) {
-    element.addEventListener('click', () => {
-      const currentUrl = window.location.href;
-      const url = new URL(currentUrl);
-      const pathname = url.pathname;
-      const searchParams = url.searchParams;
-
-      const xEventMap = {
-        '/intent/retweet': {
-          searchParamKey: 'tweet_id',
-          eventName: 'repost',
-        },
-        '/intent/follow': {
-          searchParamKey: 'screen_name',
-          eventName: 'follow',
-        },
-      };
-      let type = 'xPage';
-      if (Object.keys(xEventMap).includes(pathname)) {
-        const { searchParamKey, eventName } = xEventMap[pathname];
-        const specificParamValue = searchParams.get(searchParamKey);
+  var cancelElement = document.querySelector(
+    "[data-testid='confirmationSheetCancel']"
+  );
+  console.log(
+    'primus injected xPage checkTimer follow btn',
+    element,
+    cancelElement
+  );
+  if (element && cancelElement) {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    const pathname = url.pathname;
+    const searchParams = url.searchParams;
+    if (Object.keys(xEventMap).includes(pathname)) {
+      const { searchParamKey, eventName } = xEventMap[pathname];
+      const specificParamValue = searchParams.get(searchParamKey);
+      element.addEventListener('click', () => {
         chrome.runtime.sendMessage({
           type,
           name: eventName,
           params: {
             [searchParamKey]: specificParamValue,
+            result: true,
           },
         });
         //   {
@@ -39,8 +47,18 @@ checkTimer = setInterval(() => {
         //     screen_name: specificParamValue,
         //   },
         // }
-      }
-    });
+      });
+      cancelElement.addEventListener('click', () => {
+        chrome.runtime.sendMessage({
+          type,
+          name: eventName,
+          params: {
+            [searchParamKey]: specificParamValue,
+            result: false,
+          },
+        });
+      });
+    }
     clearInterval(checkTimer);
   }
 }, 200);
