c65fb4d75920272c70b5e2d2bacec4cd33f14874
ysm <ysm@ysm.com>
2026-06-04 11:59:01 +0800

    chore: update version to 0.4.5 & update zip file name

--- utils/build.js
+++ utils/build.js
@@ -15,9 +15,18 @@ config.mode = 'production';
 
 var packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 
+function getLocalTimestamp() {
+  const pad = (num) => String(num).padStart(2, '0');
+  const now = new Date();
+
+  return `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(
+    now.getDate()
+  )}-${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`;
+}
+
 config.plugins = (config.plugins || []).concat(
   new ZipPlugin({
-    filename: `${packageInfo.name}-${packageInfo.version}.zip`,
+    filename: `${packageInfo.name}-${packageInfo.version}-${getLocalTimestamp()}.zip`,
     path: path.join(__dirname, '../', 'zip'),
   })
 );
