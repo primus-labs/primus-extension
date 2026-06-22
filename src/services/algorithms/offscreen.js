c86a115262585a1df27c5d1b9187dfc2967d5bfa
fksyuan <fksyuan@163.com>
2026-06-22 15:12:29 +0800

    update algorithm sdk

--- src/services/algorithms/offscreen.js
+++ src/services/algorithms/offscreen.js
@@ -14,7 +14,7 @@ Module.onRuntimeInitialized = async () => {
   });
 };
 
-const CLIENT_VERSION = '1.4.26';
+const CLIENT_VERSION = '1.4.33';
 const LEGACY_NETWORK_VERSION = '1.1.1';
 const SDK_ATTESTATION_SESSION_KEY = 'padoZKAttestationJSSDKSession';
 
